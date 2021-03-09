296234134f225f9bb03d79ec93fb76be0786abfd
Sébastien Weber <seba.weber@gmail.com>
2021-03-09 17:18:20 +0100

    Update daq_1Dviewer_Femto.py

--- src/pymodaq_plugins_femto/daq_viewer_plugins/plugins_1D/daq_1Dviewer_Femto.py
+++ src/pymodaq_plugins_femto/daq_viewer_plugins/plugins_1D/daq_1Dviewer_Femto.py
@@ -1,6 +1,7 @@
 import numpy as np
 from easydict import EasyDict as edict
 from pymodaq.daq_utils.daq_utils import ThreadCommand, getLineInfo, DataFromPlugins, Axis, gauss1D, normalize
+from pymodaq.daq_utils.array_manipulation import crop_vector_to_axis, linspace_this_vect
 from pymodaq.daq_viewer.utility_classes import DAQ_Viewer_base, comon_parameters
 from pymodaq.daq_utils.parameter.utils import iter_children, get_param_path
 from pathlib import Path
@@ -15,13 +16,13 @@ class DAQ_1DViewer_Femto(DAQ_Viewer_base):
     params = comon_parameters + \
         [{'title': 'Simulation settings:', 'name': 'simul_settings', 'type': 'group',
             'children': [
-                {'title': '', 'name': 'show_pulse_bool', 'type': 'bool_push', 'label': 'Show Pulse'},
-                {'title': '', 'name': 'show_trace_bool', 'type': 'bool_push', 'label': 'Show Trace'},
+                {'title': 'Show Spectrum:', 'name': 'show_pulse_bool', 'type': 'bool'},
+                {'title': 'Show Trace:', 'name': 'show_trace_bool', 'type': 'bool'},
             ] + Simulator.params}, ] + \
         [{'title': 'Spectrometer settings:', 'name': 'spectro_settings', 'type': 'group', 'children': [
-             {'title': 'Min Wavelength (nm):', 'name': 'wl_min', 'type': 'float', 'value': 250,
+             {'title': 'Min Wavelength (nm):', 'name': 'wl_min', 'type': 'float', 'value': 500,
               'tip': 'Minimal Wavelength of the virtual spectrometer'},
-             {'title': 'Max Wavelength (nm):', 'name': 'wl_max', 'type': 'float', 'value': 1000,
+             {'title': 'Max Wavelength (nm):', 'name': 'wl_max', 'type': 'float', 'value': 1100,
               'tip': 'Minimal Wavelength of the virtual spectrometer'},
 
              {'title': 'Npoints:', 'name': 'npoints_spectro', 'type': 'list',
@@ -43,51 +44,21 @@ class DAQ_1DViewer_Femto(DAQ_Viewer_base):
         self.max_pnps = None
         self.spectro_wavelength = None
 
+
     def commit_settings(self, param):
         """
         """
+        if param.name() == 'show_trace_bool':
+            self.settings.child('simul_settings', 'show_pulse_bool').setValue(False)
+        elif param.name() == 'show_pulse_bool':
+            self.settings.child('simul_settings', 'show_trace_bool').setValue(False)
 
-        if param.name() == 'show_pulse_bool':
-            self.controller.update_pulse()
-
-            intensity_t = lib.abs2(self.controller.pulse.field)
-            intensity_t = intensity_t / np.max(intensity_t)
-            phaset = np.unwrap(np.angle(self.controller.pulse.field))
-            phaset -= lib.mean(phaset, intensity_t**2)
-
-            intensity_w = lib.abs2(self.controller.pulse.spectrum)
-            intensity_w = intensity_w / np.max(intensity_w)
-            phasew = np.unwrap(np.angle(self.controller.pulse.spectrum))
-            phasew -= lib.mean(phasew, intensity_w**2)
-
-            self.data_grabed_signal_temp.emit([
-                DataFromPlugins(name='FemtoPulse Temporal',
-                                data=[intensity_t, phaset],
-                                dim='Data1D', labels=['Intensity', 'Phase'],
-                                x_axis=Axis(data=self.controller.pulse.t * 1e15, label='time', units='fs')),
-                DataFromPlugins(name='FemtoPulse Spectral',
-                                data=[intensity_w, phasew],
-                                dim='Data1D', labels=['Amplitude', 'Phase'],
-                                x_axis=Axis(data=convert(self.controller.pulse.w + self.controller.pulse.w0, "om", "wl"),
-                                            label='Wavelength', units='nm'))
-            ])
-        elif param.name() == 'show_trace_bool':
-            self.controller.update_pnps()
-            self.data_grabed_signal_temp.emit([
-                DataFromPlugins(name='Full Trace',
-                                data=[normalize(np.fliplr(self.controller.trace.data.T) / self.controller.max_pnps)],
-                                dim='Data2D', labels=['NL trace'],
-                                x_axis=Axis(data=self.controller.trace.axes[0][::-1] * 1e15,
-                                            label=self.controller.trace.labels[0], units='fs'),
-                                y_axis=Axis(data=self.controller.trace.axes[1],
-                                            label=self.controller.trace.labels[1], units='Hz')
-                                ),])
-            
         elif param.name() in iter_children(self.settings.child('spectro_settings'), []):
             self.update_spectro()
             
         elif param.name() in iter_children(self.settings.child('simul_settings'), []):
             self.controller.settings.child(*get_param_path(param)[3:]).setValue(param.value())
+            self.controller.update_pnps()
 
 
     def update_spectro(self):
@@ -142,6 +113,7 @@ class DAQ_1DViewer_Femto(DAQ_Viewer_base):
             self.get_measure_axis()
             self.emit_x_axis()
             self.update_spectro()
+            self.controller.update_pnps()
 
             ##############################
 
@@ -171,22 +143,62 @@ class DAQ_1DViewer_Femto(DAQ_Viewer_base):
         Naverage: (int) Number of hardware averaging
         kwargs: (dict) of others optionals arguments
         """
-        if 'positions' in kwargs:
-            parameter = kwargs['positions'][0] * 1e-15
-        else:
-            parameter = self.settings.child('param_val').value()
-        self.controller.pnps.calculate(self.controller.pulse.spectrum, parameter)
-        data = np.interp(self.spectro_wavelength,
-                         np.flip(self.controller.pnps.process_wl * 1e9),
-                         np.flip(self.controller.pnps.Tmn / self.controller.max_pnps))
+        if self.settings.child('simul_settings', 'show_pulse_bool').value():
+            self.controller.update_pulse()
+            w = self.controller.pulse.w
+            spectrum = self.controller.pulse.spectrum
 
-        self.data_grabed_signal.emit([
-            DataFromPlugins(name='PNPS',
-                            data=[data],
-                            dim='Data1D', labels=['NL trace'],
-                            x_axis=Axis(data=self.spectro_wavelength,
-                                        label='Wavelength', units='nm')
-                            ), ])
+            intensity_w = lib.abs2(spectrum)
+            intensity_w = intensity_w / np.max(intensity_w)
+            wl_lim = self.settings.child('spectro_settings', 'wl_min').value() * 1e-9, \
+                     self.settings.child('spectro_settings', 'wl_max').value() * 1e-9
+            wl = convert(w + self.controller.pulse.w0, "om", "wl")
+
+            wlc, intensity_croped = crop_vector_to_axis(wl[::-1], intensity_w[::-1], wl_lim)
+            wlc_lin, intensity_lin = linspace_this_vect(wlc, intensity_croped,
+                                                       self.settings.child('spectro_settings',
+                                                                           'npoints_spectro').value())
+
+            self.data_grabed_signal.emit([
+                DataFromPlugins(name='FemtoPulse Spectral',
+                                data=[intensity_lin],
+                                dim='Data1D', labels=['Spectral Intensity'],
+                                x_axis=Axis(data=wlc_lin,
+                                            label='Wavelength', units='m'))
+            ])
+        elif self.settings.child('simul_settings', 'show_trace_bool').value():
+            self.controller.update_pnps()
+            wl_lim = self.settings.child('spectro_settings', 'wl_min').value() * 1e-9, \
+                     self.settings.child('spectro_settings', 'wl_max').value() * 1e-9
+            Npts = self.settings.child('spectro_settings',
+                                'npoints_spectro').value()
+            md = self.controller.trace_exp(Npts=Npts, wl_lim=wl_lim)
+            self.data_grabed_signal.emit([
+                DataFromPlugins(name='Full Trace',
+                                data=[md.data.T],
+                                dim='Data2D', labels=['NL trace'],
+                                x_axis=Axis(data=md.axes[0],
+                                            label=md.labels[0], units=md.units[0]),
+                                y_axis=Axis(data=md.axes[1],
+                                            label=md.labels[1], units=md.units[1])
+                                ),])
+        else:
+            if 'positions' in kwargs:
+                parameter = kwargs['positions'][0] * 1e-15
+            else:
+                parameter = self.settings.child('param_val').value()
+            self.controller.pnps.calculate(self.controller.pulse.spectrum, parameter)
+            data = np.interp(self.spectro_wavelength,
+                             np.flip(self.controller.pnps.process_wl * 1e9),
+                             np.flip(self.controller.pnps.Tmn / self.controller.max_pnps))
+
+            self.data_grabed_signal.emit([
+                DataFromPlugins(name='PNPS',
+                                data=[data],
+                                dim='Data1D', labels=['NL trace'],
+                                x_axis=Axis(data=self.spectro_wavelength,
+                                            label='Wavelength', units='nm')
+                                ), ])
 
 
 
