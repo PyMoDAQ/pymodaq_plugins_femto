4f4209f5f573e79223bb66a3de35bbc906aead57
Sébastien Weber <seba.weber@gmail.com>
2022-10-21 21:13:46 +0200

    update github org name

--- README.rst
+++ README.rst
@@ -9,8 +9,8 @@ pymodaq_plugins_femto (Femto Characterization)
    :target: https://pymodaq.readthedocs.io/en/stable/?badge=latest
    :alt: Documentation Status
 
-.. image:: https://github.com/CEMES-CNRS/pymodaq_plugins_femto/workflows/Upload%20Python%20Package/badge.svg
-    :target: https://github.com/CEMES-CNRS/pymodaq_plugins_femto
+.. image:: https://github.com/PyMoDAQ/pymodaq_plugins_femto/workflows/Upload%20Python%20Package/badge.svg
+    :target: https://github.com/PyMoDAQ/pymodaq_plugins_femto
 
 Plugins wrapping the Pypret package to simulate femtosecond pulse characterization
 
--- src/pymodaq_plugins_femto/daq_viewer_plugins/plugins_1D/daq_1Dviewer_Femto.py
+++ src/pymodaq_plugins_femto/daq_viewer_plugins/plugins_1D/daq_1Dviewer_Femto.py
@@ -2,7 +2,7 @@ import numpy as np
 from easydict import EasyDict as edict
 from pymodaq.daq_utils.daq_utils import ThreadCommand, getLineInfo, DataFromPlugins, Axis, gauss1D, normalize
 from pymodaq.daq_utils.array_manipulation import crop_vector_to_axis, linspace_this_vect
-from pymodaq.daq_viewer.utility_classes import DAQ_Viewer_base, comon_parameters
+from pymodaq.control_modules.viewer_utility_classes import DAQ_Viewer_base, comon_parameters
 from pymodaq.daq_utils.parameter.utils import iter_children, get_param_path
 from pathlib import Path
 from pypret.frequencies import om2wl, wl2om, convert
